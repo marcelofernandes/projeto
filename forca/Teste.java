25eeaca719e1af57928d0c2585f8f37fc5b9eb03
unknown <Aluno@L2C49.(none)>
2013-01-28 14:57:43 -0300

    adicionando arquivo de teste

---
+++ forca/Teste.java
@@ -0,0 +1,23 @@
+package br.ufpb.poo.forca;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class Teste {
+	
+	@Test
+	public void testaGerenteDePersistencia(){
+		GerenteDePersistencia gerente = new GerenteDePersistencia();
+		assertTrue(gerente.getFrase()=="");
+		assertTrue(gerente.getPalavraIngles()=="");
+		assertEquals(gerente.getPalavraPortugues(),"");
+	}
+	
+	@Test
+	public void testaDesafio(){
+		Desafio desafio = new Desafio("ingles","portugues","frase");
+		assertTrue(desafio.getFrase()=="frase");
+		assertTrue(desafio.getPalavrasPortugues().equals("portugues"));
+		assertEquals(desafio.getPalavraIngles(),"ingles");
+	}
+}
