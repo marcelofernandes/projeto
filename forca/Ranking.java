e1a4bb1b07cfe24193788510541cb254fb5c6903
marcelofernandes <marcelo.fernandes@dce.ufpb.br>
2013-01-28 14:45:55 -0200

    Update forca/Ranking.java

--- forca/Ranking.java
+++ forca/Ranking.java
@@ -1,5 +1,28 @@
 package Projeto_de_ayla;
 
-public class Ranking {
+import java.util.Arrays;
 
+public class Ranking {
+  
+	int pontos []; 
+	 
+	
+	public Ranking(){
+		pontos = new int [4]; 
+	}
+
+	public void pontuacaoJogador(int []pontuacao){
+		for(int i=0; i<pontos.length; ++i){
+			pontos[i]= pontuacao[i];
+			}
+	}
+	
+	public void ranking(){
+		Arrays.sort(pontos,0,pontos.length);
+		System.out.println(Arrays.toString(pontos));
+	}
+		
+	
+	
+	}
 }
