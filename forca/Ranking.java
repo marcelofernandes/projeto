4feb7221989864c844d826020c08f87b53b4f7a9
leonardo <leonardo.costa@dce.ufpb.br>
2013-01-28 15:49:15 -0200

    Update forca/Ranking.java

--- forca/Ranking.java
+++ forca/Ranking.java
@@ -3,25 +3,29 @@ package Projeto_de_ayla;
 import java.util.Arrays;
 
 public class Ranking {
-  
-	int pontos []; 
-	 
-	
+	  
+	int pontos [];
+
+
 	public Ranking(){
-		pontos = new int [4]; 
+		pontos = new int [4];
 	}
 
 	public void pontuacaoJogador(int []pontuacao){
 		for(int i=0; i<pontos.length; ++i){
 			pontos[i]= pontuacao[i];
-			}
+		}
 	}
-	
-	public void ranking(){
+
+	public void PontuacoesOrdenadas(){
 		Arrays.sort(pontos,0,pontos.length);
+		
+	}
+	public void imprimnirRanking(){
+		PontuacoesOrdenadas();
 		System.out.println(Arrays.toString(pontos));
 	}
-		
-	
+
 
 }
+
