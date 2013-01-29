2ba86d48d360fbc79c2b516eec996aafedc5d235
marcelofernandes <marcelo.fernandes@dce.ufpb.br>
2013-01-28 22:12:42 -0200

    Update forca/Facade.java

--- forca/Facade.java
+++ forca/Facade.java
@@ -1,3 +1,10 @@
 public class Facade{
-
+  private GerenteDePersistencia gerente;
+  
+  public Facade(){
+    gerente = new GerenteDePersistencia();
+  }
+  public Desafio ObterDesafio(){
+    return new Desafio(gerente.getPalavraIngles(), gerente.getPalavraPortugues(), gerente.getFrase());
+  }
 }
