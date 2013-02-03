191bc54ad43b38afaaa391b7b868a4fff51bacc3
leonardo <leonardo.costa@dce.ufpb.br>
2013-02-03 13:05:21 -0200

    Update forca/TesteRanking.java

--- forca/TesteRanking.java
+++ forca/TesteRanking.java
@@ -1,56 +1,51 @@
 package Projeto_de_ayla;
 
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
 
 public class TesteRanking {
 	
-	Ranking2 ranking = new Ranking2(); 
+	private Ranking ranking = new Ranking(); 
+	private String []nomes = {"Lucas","Pedro","Ana","Maria","Jorge","Ricardo",
+			"Eduardo","Carlos","Gil","Rafael","J√∫lia","Carla"};
+	private Jogador []jogadores = new Jogador[12];
+	private int [] pontos = {200,100,50,150,560,670,890,432,765,78,324,760};
+	
 	
 	@Test
 	public void TesteRanking(){
-		
-		//verificando se o vetor est· vazio.
-		assertEquals(0,ranking.pontosDoJogador[0]);
-		assertEquals(0,ranking.pontosDoJogador[3]);
-		
-		//pontuacao do jogador
-		ranking.pontuacaoDoJogador(76);
-		ranking.pontuacaoDoJogador(5);
-		ranking.pontuacaoDoJogador(1);
-		ranking.pontuacaoDoJogador(98);
-		
-		
-		
-						
-		// verificar se est· ordenando o vetor.
-		ranking.rankingOrdenado();
-		assertEquals(1, ranking.pontosDoJogador[0]);
-		assertEquals(5, ranking.pontosDoJogador[1]);
-		assertEquals(76, ranking.pontosDoJogador[2]);
-		assertEquals(98, ranking.pontosDoJogador[3]);
 	
-		//testando maior pontuaÁ„o
-		assertEquals(98, ranking.getMaiorPontuacao());
+		for(int i=0;i<nomes.length;i++){
+			jogadores[i]= new Jogador(nomes[i]);
+			jogadores[i].setPontuacao(pontos[i]);
+			ranking.compararJogadorComRanking(jogadores[i]);
+		}
 		
-		//testando saÌda do ranking
-		int vetor[] = ranking.ranking();
-		assertEquals(1, ranking.pontosDoJogador[0]);
-		assertEquals(5, ranking.pontosDoJogador[1]);
-		assertEquals(76, ranking.pontosDoJogador[2]);
-		assertEquals(98, ranking.pontosDoJogador[3]);
+		//testando o m√©todo de ordena√ß√£o dos jogadores
+		List <Jogador> busca =  ranking.getDadosDoRanking();
 		
-		
-		
-		
-		
+		//testando o nome e a pontua√ß√£o do jogador do topo da lista
+		assertEquals("Eduardo",busca.get(0).getNome());
+		assertEquals(890,busca.get(0).getPontuacao());
 		
 		
+		//testando o m√©todo de compara√ß√£o de jogadores
+		Jogador jogadorMaior = new Jogador("Fernando");
+		jogadorMaior.setPontuacao(1000);
+		
+		ranking.compararJogadorComRanking(jogadorMaior);	
+		
+		assertEquals("Fernando",busca.get(0).getNome());
+		assertEquals(1000,busca.get(0).getPontuacao());
 		
 		
-	}
+			
+			
+		}
 	
 
 }
