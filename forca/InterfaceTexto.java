7f45720acfb8e2c806a61157e91835fa52401614
marcelo de melo fernandes <marcelo.fernandes@dce.ufpb.br>
2013-02-03 20:17:18 -0300

    adicionando InterfaceTexto

---
+++ forca/InterfaceTexto.java
@@ -0,0 +1,20 @@
+public class InterfaceTexto implements InterfaceComUsuario{
+
+	public void mostrarMenu(){
+		//TODO
+	}
+
+	public void escolherNivel(){
+		//TODO
+	}
+
+	public void nomeDoJogador(){
+		//TODO
+	}
+
+	public void iniciarJogo(){
+		//TODO
+	}
+
+
+}
