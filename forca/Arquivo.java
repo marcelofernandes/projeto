9ec5fdc1b91c40fffd9ecfc28b99c089f3189b19
marcelo de melo fernandes <marcelo.fernandes@dce.ufpb.br>
2013-02-03 20:10:43 -0300

    adicionando arquivo

---
+++ forca/Arquivo.java
@@ -0,0 +1,25 @@
+public class Arquivo implements PersistenciaInterface{
+	
+	public Arquivo(){
+	}
+	public String getPalavraPortugues(){
+		//TODO
+	}
+
+	public String getPalavraIngles(){
+		//TODO
+	}
+
+	public String getFrase(){
+		//TODO
+	}
+
+	public String getAudioDaPalavra(){
+		//TODO
+	}
+
+	public String getAudioDaFrase(){
+		//TODO
+	}
+
+}
