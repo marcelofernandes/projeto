7e5696dd6df46eeb614d6659b21fb3e260332f4a
marcelo de melo fernandes <marcelo.fernandes@dce.ufpb.br>
2013-02-03 20:16:38 -0300

    adicionando InterfaceGUI

---
+++ forca/InterfaceGUI.java
@@ -0,0 +1,19 @@
+public class InterfaceGUI implements InterfaceComUsuario{
+
+	public void mostrarMenu(){
+		//TODO
+	}
+
+	public void escolherNivel(){
+		//TODO
+	}
+
+	public void nomeDoJogador(){
+		//TODO
+	}
+
+	public void iniciarJogo(){
+		//TODO
+	}
+
+}
