7865b8aaafde2fccd7f0b79b5c1589f0068de302
marcelo de melo fernandes <marcelo.fernandes@dce.ufpb.br>
2013-02-03 20:19:55 -0300

    adicionando persistenciainterface

---
+++ forca/PersistenciaInterface.java
@@ -0,0 +1,9 @@
+public class PersistenciaInterface{
+
+	public String getPalavraPortugues();
+	public String getPalavraIngles();
+	public String getFrase();
+	public String getAudioDaPalavra();
+	public String getAudioDaFrase();
+
+}
