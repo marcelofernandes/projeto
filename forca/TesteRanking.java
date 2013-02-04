f35139e718277e1f24a464efa43f63e0135313cd
unknown <Aluno@L2C46.(none)>
2013-02-04 15:01:44 -0300

    testando git

--- forca/TesteRanking.java
+++ forca/TesteRanking.java
@@ -1,11 +1,11 @@
-package Projeto_de_ayla;
+﻿package Projeto_de_ayla;
 
 import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
-
+//teste
 
 public class TesteRanking {
 	
