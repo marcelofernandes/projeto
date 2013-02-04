dc4388e1d8c76dc70c5cf3743fbfdd94e584287c
unknown <Aluno@L2C46.(none)>
2013-02-04 14:49:14 -0300

    add time

---
+++ forca/Time.java
@@ -0,0 +1,57 @@
+package Projeto_de_ayla;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.util.Date;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class Time extends JPanel implements Runnable{
+	public static void main(String []args){
+		new Thread(new Time()).start();
+			
+	}
+	
+	
+	private int tempoDecorrido = 60;
+	JFrame frame; 
+		
+	public Time(){
+		int largura = Toolkit.getDefaultToolkit().getScreenResolution();  
+		int altura = Toolkit.getDefaultToolkit().getScreenResolution(); 
+		this.frame = new JFrame();
+		frame.add(this);
+		frame.setSize(altura,largura);
+		frame.setResizable(false);  
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	public void paintComponent(Graphics g){ 
+		  super.paintComponent(g);
+	      g.setFont(new Font("Arial", Font.ITALIC, 30));  
+	      g.drawString(""+tempoDecorrido , 10, 40);  
+	      tempoDecorrido--; //diminui o tempo decorrido
+	   }  
+	     
+	 public void run(){  
+	                    
+	      while(true){  
+	           
+	         try{  
+	            Thread.sleep(1000);              
+	            repaint();                          
+	         }  
+	         catch(InterruptedException ie){}     
+	      }        
+	   }     
+	     
+	}   
+
+	
+
+	
