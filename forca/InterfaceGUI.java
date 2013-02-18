c6361f00b830e612f1a369e9d57b0601a5594e81
marcelofernandes <marcelo.fernandes@dce.ufpb.br>
2013-02-18 17:39:12 -0300

    Update forca/InterfaceGUI.java

--- forca/InterfaceGUI.java
+++ forca/InterfaceGUI.java
@@ -37,4 +37,9 @@ public class InterfaceGUI implements InterfaceComUsuario{
 		return 0;
 	}
 
+	@Override
+	public void imprimirRanking(List<Jogador> dadosDoRanking) {
+		// TODO Auto-generated method stub
+		
+	}
 }
